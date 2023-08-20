31141c7f5814a49ec5f32f0b31d9cde65639f04e
shashank-iter <pandeyshashank812@gmail.com>
2023-08-21 03:48:03 +0530

    supabase integration, invoiceData saved in backend

--- src/pages/invoiceAdd/index.jsx
+++ src/pages/invoiceAdd/index.jsx
@@ -5,19 +5,80 @@ import Textinput from "@/components/ui/Textinput";
 import Textarea from "@/components/ui/Textarea";
 import Repeater from "../repeater/Repeater";
 import Flatpickr from "react-flatpickr";
-
+import { v5 as uuidv5 } from "uuid";
+import Cookies from "js-cookie";
+import supabase from "../auth/supabaseClient";
+import Swal from "sweetalert2";
 const InvoiceAddPage = () => {
   const [picker, setPicker] = useState(new Date());
+  const [invoiceId, setInvoiceId] = useState(
+    uuidv5("https://example.com/name", uuidv5.URL)
+  );
+  const [itemData, setItemData] = useState([]);
+  const [invoiceData, setInvoiceData] = useState({
+    itemData: {},
+    creationDate: picker,
+  });
+  const sendData = (data) => {
+    setItemData(data);
+    // console.log(itemData);
+  };
+
+
+    function handleChange(e) {
+      setInvoiceData((prevInvoiceData) => ({
+        ...prevInvoiceData,
+        [e.target.name]: e.target.value,
+      }));
+      // console.log(invoiceData);
+    }
+  
+
+  const ownerData ={
+    ownerEmail: Cookies.get("email"),
+    
+  }
+
+  async function createInvoice() {
+    try{
+      const { data, error } = await supabase
+      .from('invoices')
+      .insert([
+        { userId: Cookies.get('userID'), invoiceData: invoiceData },
+      ])
+      .select()
+      Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'Your work has been saved',
+        showConfirmButton: false,
+        timer: 1500
+      })
+    }
+    catch(error){
+      Swal.fire({
+        position: 'center',
+        icon: 'error',
+        title: 'Oops...Something went wrong!',
+        showConfirmButton: false,
+        timer: 1500
+      })
+    }
+
+  }
+
   return (
     <div>
       <Card title="Create new invoice">
         <h4 className="text-slate-900 dark:text-white text-xl mb-4">
-          #89572935Kh
+          <h6 className="pb-4">Invoice ID: </h6>
+
+          {invoiceId}
         </h4>
         <div className="grid lg:grid-cols-2 grid-cols-1 gap-5">
           <div className="grid lg:grid-cols-2 grid-cols-1 gap-5">
             <div className="lg:col-span-2 col-span-1 text-slate-900 dark:text-slate-300 text-base font-medium">
-              Recipient info-500
+              Recipient info
             </div>
             <div>
               <label htmlFor="default-picker" className=" form-label">
@@ -32,49 +93,75 @@ const InvoiceAddPage = () => {
               />
             </div>
 
-            <Textinput label="Name" type="text" placeholder="Add your name" />
-            <Textinput label="Phone" type="text" placeholder="Add your phone" />
             <Textinput
-              label="Email"
+              label="Recipient's Name"
+              type="text"
+              placeholder="Add Recipient's Name"
+              onChange={(e) => {
+                handleChange(e);
+                e.target.name = "recipientName";
+              }}
+
+            />
+            <Textinput label="Recipient's Phone" type="text" placeholder="Add Recipient's phone" 
+              onChange={(e) => {
+                handleChange(e);
+                e.target.name = "recipientPhone";
+              }}
+            />
+            <Textinput
+              label="Recipient's Email"
               type="email"
-              placeholder="Add your email"
+              placeholder="Add recipient's email"
+              onChange={(e) => {
+                handleChange(e);
+                e.target.name = "recipientEmail";
+              }}
             />
             <div className="lg:col-span-2 col-span-1">
               <Textarea
-                label="Address"
-                type="email"
-                placeholder="address"
+                label="Recipient's Address"
+                type="text"
+                placeholder="Add Recipient's Address"
                 rows="2"
+                onChange={(e) => {
+                handleChange(e);
+                e.target.name = "recipientAddress";
+              }}
               />
             </div>
           </div>
           <div className="grid lg:grid-cols-2 grid-cols-1 gap-5">
             <div className="lg:col-span-2 col-span-1 text-slate-900 text-base dark:text-slate-300 font-medium">
-              Owner info-500
+              Owner info
             </div>
 
             <Textinput label="Name" type="text" placeholder="Add your name" />
             <Textinput label="Phone" type="text" placeholder="Add your phone" />
             <div className="lg:col-span-2 col-span-1">
               <Textinput
-                label="Email"
+                label="Ower's Email"
                 type="email"
                 placeholder="Add your email"
+                onChange={(e) => {
+                handleChange(e);
+                e.target.name = "ownerEmail";
+              }}
               />
             </div>
 
             <div className="lg:col-span-2 col-span-1">
               <Textarea
-                label="Address"
-                type="email"
-                placeholder="address"
+                label="Owner's Address"
+                type="text"
+                placeholder="Add your address"
                 rows="2"
               />
             </div>
           </div>
         </div>
         <div className="my-6">
-          <Repeater />
+          <Repeater sendData={sendData} />
         </div>
         <Textarea
           label="Additional note"
@@ -82,9 +169,17 @@ const InvoiceAddPage = () => {
           rows="2"
           placeholder="Note"
           className="lg:w-1/2"
+          onChange={(e) => {
+                handleChange(e);
+                e.target.name = "additionalNote";
+              }}
         />
-        <div className="ltr:text-right rtl:text-left space-x-3 rtl:space-x-reverse">
-          <Button text="Save" className="btn-dark" />
+        <div className="ltr:text-right rtl:text-left space-x-3 rtl:space-x-reverse mt-4">
+          <Button text="Save" className="btn-dark" onClick={()=>{
+            invoiceData.itemData = itemData;
+            createInvoice();
+            // console.log(invoiceData);
+          }} />
         </div>
       </Card>
     </div>

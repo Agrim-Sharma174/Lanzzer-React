ff9cb25183798765682968b5b11de77b0cedf750
shashank-iter <pandeyshashank812@gmail.com>
2023-08-20 15:10:43 +0530

    hardcode url removed

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -9,15 +9,7 @@ const Login = () => {
   //SearchParam : 
   // this function checks if the user is logged in or not
 useEffect(() => {
-
-    supabase.auth.onAuthStateChange((event, session) => {
-        if (event == 'SIGNED_IN'){
-            console.log('signed in')
-            // Cookies.set('sb-access-token', session.access_token)
-            // Cookies.set('sb-refresh-token', session.refresh_token)
-            // window.location.href = '/dashboard'
-        }
-      })
+   
 }, []);
 
 
@@ -34,13 +26,19 @@ useEffect(() => {
             access_type: "offline",
             prompt: "consent",
           },
-          redirectTo: "http://localhost:5173/dashboard",
+          redirectTo: "https://lanzzer-react.vercel.app/dashboard",
 
         },
 
       });
-      console.log(data);
-   
+      supabase.auth.onAuthStateChange((event, session) => {
+        if (event == 'SIGNED_IN'){
+            console.log('signed in')
+            Cookies.set('sb-access-token', session.access_token)
+            Cookies.set('sb-refresh-token', session.refresh_token)
+            window.location.href = '/dashboard'
+        }
+      })
     //   checkUser();
     //   Navigate("/dashboard");
 

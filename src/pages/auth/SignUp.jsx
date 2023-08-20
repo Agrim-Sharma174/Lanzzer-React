6137365164719edec35af0d4db4d738c1640ec69
shashank-iter <pandeyshashank812@gmail.com>
2023-08-20 14:09:00 +0530

    page link to login

--- src/pages/auth/SignUp.jsx
+++ src/pages/auth/SignUp.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const SignUp = () => {
   return (
@@ -39,9 +40,12 @@ const SignUp = () => {
                       </svg>
                       <span>Sign Up</span>
                 </button>
-                <p class="text-center">Alreday have an account ? <a href="#" class="text-indigo-600 font-medium inline-flex space-x-1 items-center"><span>Login </span><span><svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
-                  </svg></span></a></p>
+                <p class="text-center">Alreday have an account ? <a href="#" class="text-indigo-600 font-medium inline-flex space-x-1 items-center">
+                <Link to="/">
+                <span>Login </span><span><svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+                  </svg></span>
+                  </Link>
+                  </a></p>
             </div>
         </form>
     </div>

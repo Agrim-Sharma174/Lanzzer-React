2015ab6e3e5dc64ccc70e5b82a200392b17a7bab
shashank-iter <pandeyshashank812@gmail.com>
2023-08-22 00:46:08 +0530

    display table added with functionality

--- src/pages/invoiceDisplay/index.jsx
+++ src/pages/invoiceDisplay/index.jsx
@@ -1,445 +1,177 @@
 import React, { useState, useMemo, useEffect } from "react";
 import { advancedTable } from "../../constant/table-data";
 import Card from "@/components/ui/Card";
-import Icon from "@/components/ui/Icon";
+import { Icon } from "@iconify/react";
+import Tooltip from "@/components/ui/Tooltip";
 import Dropdown from "@/components/ui/Dropdown";
 import Button from "@/components/ui/Button";
 import supabase from "../auth/supabaseClient";
 import Cookies from "js-cookie";
 import { useNavigate } from "react-router-dom";
-import {
-  useTable,
-  useRowSelect,
-  useSortBy,
-  useGlobalFilter,
-  usePagination,
-} from "react-table";
-import GlobalFilter from "../globalFilter";
-
-const IndeterminateCheckbox = React.forwardRef(
-  ({ indeterminate, ...rest }, ref) => {
-    const defaultRef = React.useRef();
-    const resolvedRef = ref || defaultRef;
-
-    React.useEffect(() => {
-      resolvedRef.current.indeterminate = indeterminate;
-    }, [resolvedRef, indeterminate]);
-  
-    return (
-      <>
-        <input
-          type="checkbox"
-          ref={resolvedRef}
-          {...rest}
-          className="table-checkbox"
-        />
-      </>
-    );
-  }
-);
-
+import Loading from "@/components/Loading";
+import { Link } from "react-router-dom";
+import InvoiceAdd from "../invoiceAdd";
+import InvoicePreview from "../invoicePreview";
 const InvoiceDisplay = () => {
   const navigate = useNavigate();
   const [invoiceData, setInvoiceData] = useState();
 
   async function getInvoice() {
-
-    try{
+    try {
       let { data: invoices, error } = await supabase
-      .from('invoices')
-      .select('*')
-      .range(0, 9)
-      .ilike('userId', `%${Cookies.get('userID')}%`)
-      // const invoices = supabase.channel('custom-filter-channel')
-      // .on(
-      //   'postgres_changes',
-      //   { event: '*', schema: 'public', table: 'invoices', filter: `userId=eq.${Cookies.get('userID')}` },
-      //   (payload) => {
-      //     console.log('Change received!', payload)
-      //   }
-      // )
-      // .subscribe()
-      setInvoiceData(invoices);
-      console.log(invoices)
-    }
-    catch(error){
-      console.log(error)
-    }
+        .from("invoices")
+        .select("*")
+        .range(0, 9)
+        .ilike("userId", `%${Cookies.get("userID")}%`);
 
+      console.log(invoices);
+
+      setInvoiceData(invoices);
+      // setTimeout(() => {
+      //   setInvoiceData(invoices);
+      // }, 5000);
+    } catch (error) {
+      console.log(error);
+    }
   }
   useEffect(() => {
     getInvoice();
   }, []);
 
-  const actions = [
+  const columns = [
     {
-      name: "send",
-      icon: "ph:paper-plane-right",
-      doit: () => {
-        navigate("/invoices/add");
-      },
+      label: "Invoice ID",
     },
     {
-      name: "view",
-      icon: "heroicons-outline:eye",
-      doit: () => {
-        navigate("/invoices/preview");
-      },
+      label: "Invoice Date",
     },
     {
-      name: "edit",
-      icon: "heroicons:pencil-square",
-      doit: (id) => {
-        navigate("/invoices/edit");
-      },
+      label: "Customer Name",
     },
     {
-      name: "delete",
-      icon: "heroicons-outline:trash",
-      doit: (id) => {
-        return null;
-      },
-    },
-  ];
-  
-  const COLUMNS = [
-    {
-      Header: "Id",
-      accessor: "id",
-      Cell: (invoiceData) => {
-        return <span>{invoiceData?.userId}</span>;
-      },
+      label: "Total Amount",
     },
     {
-      Header: "Order",
-      accessor: "order",
-      Cell: (r) => {
-        return <span>#{r?.cell?.value}</span>;
-      },
+      label: "Preview",
     },
     {
-      Header: "customer",
-      accessor: "customer",
-      Cell: (row) => {
-        return (
-          <div>
-            <span className="inline-flex items-center">
-              <span className="w-7 h-7 rounded-full ltr:mr-3 rtl:ml-3 flex-none bg-slate-600">
-                <img
-                  src={row?.cell?.value.image}
-                  alt=""
-                  className="object-cover w-full h-full rounded-full"
-                />
-              </span>
-              <span className="text-sm text-slate-600 dark:text-slate-300 capitalize">
-                {row?.cell?.value.name}
-              </span>
-            </span>
-          </div>
-        );
-      },
+      label: "Edit",
     },
     {
-      Header: "date",
-      accessor: "date",
-      Cell: (row) => {
-        return <span>{row?.cell?.value}</span>;
-      },
-    },
-    {
-      Header: "quantity",
-      accessor: "quantity",
-      Cell: (row) => {
-        return <span>{row?.cell?.value}</span>;
-      },
-    },
-    {
-      Header: "amount",
-      accessor: "amount",
-      Cell: (row) => {
-        return <span>{row?.cell?.value}</span>;
-      },
-    },
-    {
-      Header: "status",
-      accessor: "status",
-      Cell: (row) => {
-        return (
-          <span className="block w-full">
-            <span
-              className={` inline-block px-3 min-w-[90px] text-center mx-auto py-1 rounded-[999px] bg-opacity-25 ${
-                row?.cell?.value === "paid"
-                  ? "text-success-500 bg-success-500"
-                  : ""
-              } 
-            ${
-              row?.cell?.value === "due"
-                ? "text-warning-500 bg-warning-500"
-                : ""
-            }
-            ${
-              row?.cell?.value === "cancled"
-                ? "text-danger-500 bg-danger-500"
-                : ""
-            }
-            
-             `}
-            >
-              {row?.cell?.value}
-            </span>
-          </span>
-        );
-      },
-    },
-    {
-      Header: "action",
-      accessor: "action",
-      Cell: (row) => {
-        return (
-          <div>
-            <Dropdown
-              classMenuItems="right-0 w-[140px] top-[110%] "
-              label={
-                <span className="text-xl text-center block w-full">
-                  <Icon icon="heroicons-outline:dots-vertical" />
-                </span>
-              }
-            >
-              <div className="divide-y divide-slate-100 dark:divide-slate-800">
-                {actions.map((item, i) => (
-                  <div
-                    key={i}
-                    onClick={() => item.doit()}
-                    className={`
-                
-                  ${
-                    item.name === "delete"
-                      ? "bg-danger-500 text-danger-500 bg-opacity-30   hover:bg-opacity-100 hover:text-white"
-                      : "hover:bg-slate-900 hover:text-white dark:hover:bg-slate-600 dark:hover:bg-opacity-50"
-                  }
-                   w-full border-b border-b-gray-500 border-opacity-10 px-4 py-2 text-sm  last:mb-0 cursor-pointer 
-                   first:rounded-t last:rounded-b flex  space-x-2 items-center rtl:space-x-reverse `}
-                  >
-                    <span className="text-base">
-                      <Icon icon={item.icon} />
-                    </span>
-                    <span>{item.name}</span>
-                  </div>
-                ))}
-              </div>
-            </Dropdown>
-          </div>
-        );
-      },
+      label: "Delete",
     },
   ];
 
-  const columns = useMemo(() => COLUMNS, []);
-  const data = useMemo(() => advancedTable, []);
-
-  const tableInstance = useTable(
-    {
-      columns,
-      data,
-    },
-
-    useGlobalFilter,
-    useSortBy,
-    usePagination,
-    useRowSelect,
-
-    (hooks) => {
-      // hooks.visibleColumns.push((columns) => [
-      //   {
-      //     id: "selection",
-      //     Header: ({ getToggleAllRowsSelectedProps }) => (
-      //       <div>
-      //         <IndeterminateCheckbox {...getToggleAllRowsSelectedProps()} />
-      //       </div>
-      //     ),
-      //     Cell: ({ row }) => (
-      //       <div>
-      //         <IndeterminateCheckbox {...row.getToggleRowSelectedProps()} />
-      //       </div>
-      //     ),
-      //   },
-      //   ...columns,
-      // ]);
-    }
-  );
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    footerGroups,
-    page,
-    nextPage,
-    previousPage,
-    canNextPage,
-    canPreviousPage,
-    pageOptions,
-    state,
-    gotoPage,
-    pageCount,
-    setPageSize,
-    setGlobalFilter,
-    prepareRow,
-  } = tableInstance;
-
-  const { globalFilter, pageIndex, pageSize } = state;
-
-  return (
+  return !invoiceData ? (
+    <Loading />
+  ) : (
     <>
-      <Card noborder>
-        <div className="md:flex pb-6 items-center">
-          <h6 className="flex-1 md:mb-0 mb-3">Invoice</h6>
-          <div className="md:flex md:space-x-3 items-center flex-none rtl:space-x-reverse">
-            <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter} />
-            <Button
-              icon="heroicons-outline:calendar"
-              text="Select date"
-              className=" btn-outline-secondary dark:border-slate-700  text-slate-600 btn-sm font-normal dark:text-slate-300 "
-              iconClass="text-lg"
-            />
-            <Button
-              icon="heroicons-outline:filter"
-              text="Filter"
-              className=" btn-outline-secondary text-slate-600 dark:border-slate-700 dark:text-slate-300 font-normal btn-sm "
-              iconClass="text-lg"
-            />
-            <Button
-              icon="heroicons-outline:plus-sm"
-              text="Add Record"
-              className=" btn-dark font-normal btn-sm "
-              iconClass="text-lg"
-              onClick={() => {
-                navigate("/invoices/add");
-              }}
-            />
-          </div>
-        </div>
+      <Card title="Invoices" 
+       headerslot={
+          <Button
+            text="Add new"
+            icon="heroicons-outline:plus"
+            className="btn-dark"
+            onClick={() => navigate("/invoices/add")}
+          />
+        }
+      >
         <div className="overflow-x-auto -mx-6">
           <div className="inline-block min-w-full align-middle">
             <div className="overflow-hidden ">
-              <table
-                className="min-w-full divide-y divide-slate-100 table-fixed dark:divide-slate-700"
-                {...getTableProps}
-              >
-                <thead className=" border-t border-slate-100 dark:border-slate-800">
-                  {headerGroups.map((headerGroup) => (
-                    <tr {...headerGroup.getHeaderGroupProps()}>
-                      {headerGroup.headers.map((column) => (
-                        <th
-                          {...column.getHeaderProps(
-                            column.getSortByToggleProps()
-                          )}
-                          scope="col"
-                          className=" table-th "
-                        >
-                          {column.render("Header")}
-                          <span>
-                            {column.isSorted
-                              ? column.isSortedDesc
-                                ? " 🔽"
-                                : " 🔼"
-                              : ""}
-                          </span>
-                        </th>
-                      ))}
+              <table className="min-w-full divide-y divide-slate-100 table-fixed dark:divide-slate-700">
+                <thead className="bg-slate-200 dark:bg-slate-700">
+                  <tr>
+                    {columns.map((column, i) => (
+                      <th key={i} scope="col" className=" table-th ">
+                        {column.label}
+                      </th>
+                    ))}
+                  </tr>
+                </thead>
+                <tbody className="bg-white divide-y divide-slate-100 dark:bg-slate-800 dark:divide-slate-700">
+                  {invoiceData.map((row, i) => (
+                    <tr
+                      key={i}
+                      className="hover:bg-slate-200 dark:hover:bg-slate-700"
+                    >
+                      <td className="table-td">{row.invoice_id}</td>
+                      <td className="table-td">
+                        {row.invoiceData.creationDate.split("T")[0]}
+                      </td>
+                      <td className="table-td">
+                        {row.invoiceData.recipientName}
+                      </td>
+                      <td className="table-td">
+                      ₹ {row.invoiceData.itemData.reduce((a, b) => a + Number(b.price), 0)}
+                      </td>
+                      <td className="table-td align-middle">
+                        {
+                          <Link to={`/invoices/preview/${row.invoice_id}`}>
+                          <Tooltip
+                                title={
+                                  <>
+                                    <Icon
+                                      icon="heroicons-outline:eye"
+                                      className="w-5 h-5 text-slate-800 dark:text-slate-400 hover:text-slate-700 dark:hover:text-slate-300 cursor-pointer"
+                                    />
+                                  </>
+                                }
+                                content="Preview Invoice"
+                                placement="top"
+                                theme="dark"
+                                className=" dark:text-white hover:text-white dark:hover:text-black"
+                                arrow
+                              />
+                              </Link>
+                        }
+                      </td>
+                      <td className="table-td align-middle">
+                        {
+                          <Link to={`/invoices/edit/${row.invoice_id}`}>
+                          <Tooltip
+                                title={
+                                  <>
+                                    <Icon
+                                      icon="heroicons-outline:pencil-square"
+                                      className="w-5 h-5 text-slate-800 dark:text-slate-400 hover:text-slate-700 dark:hover:text-slate-300 cursor-pointer"
+                                    />
+                                  </>
+                                }
+                                content="Edit Invoice"
+                                placement="top"
+                                theme="dark"
+                                className="w-5 h-5 text-slate-800 dark:text-slate-400 hover:text-slate-700 dark:hover:text-slate-300 cursor-pointer"
+                                arrow
+                              />
+                              </Link>
+                        }
+                      </td>
+                      <td className="table-td align-middle">
+                        {
+                          <Tooltip
+                                title={
+                                  <>
+                                    <Icon
+                                      icon="heroicons-outline:trash"
+                                      className="w-5 h-5 text-red-500 dark:text-red-500 hover:text-red-600 dark:hover:text-red-600 cursor-pointer"
+                                    />
+                                  </>
+                                }
+                                content="Delete Invoice"
+                                placement="top"
+                                theme="dark"
+                                className=" dark:text-white hover:text-white dark:hover:text-black"
+                                arrow
+                              />
+                        }
+                      </td>
                     </tr>
                   ))}
-                </thead>
-                <tbody
-                  className="bg-white divide-y divide-slate-100 dark:bg-slate-800 dark:divide-slate-700"
-                  {...getTableBodyProps}
-                >
-                  {page.map((row) => {
-                    prepareRow(row);
-                    return (
-                      <tr {...row.getRowProps()}>
-                        {row.cells.map((cell) => {
-                          return (
-                            <td {...cell.getCellProps()} className="table-td">
-                              {cell.render("Cell")}
-                            </td>
-                          );
-                        })}
-                      </tr>
-                    );
-                  })}
                 </tbody>
               </table>
             </div>
           </div>
-        </div>
-        <div className="md:flex md:space-y-0 space-y-5 justify-between mt-6 items-center">
-          <div className=" flex items-center space-x-3 rtl:space-x-reverse">
-            <span className=" flex space-x-2  rtl:space-x-reverse items-center">
-              <span className=" text-sm font-medium text-slate-600 dark:text-slate-300">
-                Go
-              </span>
-              <span>
-                <input
-                  type="number"
-                  className=" form-control py-2"
-                  defaultValue={pageIndex + 1}
-                  onChange={(e) => {
-                    const pageNumber = e.target.value
-                      ? Number(e.target.value) - 1
-                      : 0;
-                    gotoPage(pageNumber);
-                  }}
-                  style={{ width: "50px" }}
-                />
-              </span>
-            </span>
-            <span className="text-sm font-medium text-slate-600 dark:text-slate-300">
-              Page{" "}
-              <span>
-                {pageIndex + 1} of {pageOptions.length}
-              </span>
-            </span>
-          </div>
-          <ul className="flex items-center  space-x-3  rtl:space-x-reverse">
-            <li className="text-xl leading-4 text-slate-900 dark:text-white rtl:rotate-180">
-              <button
-                className={` ${
-                  !canPreviousPage ? "opacity-50 cursor-not-allowed" : ""
-                }`}
-                onClick={() => previousPage()}
-                disabled={!canPreviousPage}
-              >
-                <Icon icon="heroicons-outline:chevron-left" />
-              </button>
-            </li>
-            {pageOptions.map((page, pageIdx) => (
-              <li key={pageIdx}>
-                <button
-                  href="#"
-                  aria-current="page"
-                  className={` ${
-                    pageIdx === pageIndex
-                      ? "bg-slate-900 dark:bg-slate-600  dark:text-slate-200 text-white font-medium "
-                      : "bg-slate-100 dark:bg-slate-700 dark:text-slate-400 text-slate-900  font-normal  "
-                  }    text-sm rounded leading-[16px] flex h-6 w-6 items-center justify-center transition-all duration-150`}
-                  onClick={() => gotoPage(pageIdx)}
-                >
-                  {page + 1}
-                </button>
-              </li>
-            ))}
-            <li className="text-xl leading-4 text-slate-900 dark:text-white rtl:rotate-180">
-              <button
-                className={` ${
-                  !canNextPage ? "opacity-50 cursor-not-allowed" : ""
-                }`}
-                onClick={() => nextPage()}
-                disabled={!canNextPage}
-              >
-                <Icon icon="heroicons-outline:chevron-right" />
-              </button>
-            </li>
-          </ul>
         </div>
       </Card>
     </>

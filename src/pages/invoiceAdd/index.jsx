ebc68e1bf869edee1ef216df71a102344bad3f84
Agrim <agrimsharma1714@gmail.com>
2023-08-22 10:03:41 +0530

    form errors added

--- src/pages/invoiceAdd/index.jsx
+++ src/pages/invoiceAdd/index.jsx
@@ -16,56 +16,73 @@ const InvoiceAddPage = () => {
   );
   const [itemData, setItemData] = useState([]);
   const [invoiceData, setInvoiceData] = useState({
- 
     itemData: {},
     creationDate: picker,
   });
+
+  const [formErrors, setFormErrors] = useState({
+    recipientName: false,
+    recipientPhone: false,
+    recipientEmail: false,
+    recipientAddress: false,
+    ownerName: false,
+    ownerPhone: false,
+    ownerEmail: false,
+    additionalNote: false,
+  });
+
   const sendData = (data) => {
     setItemData(data);
     // console.log(itemData);
   };
 
+  function handleChange(e) {
+    const { name, value } = e.target;
 
-    function handleChange(e) {
-      setInvoiceData((prevInvoiceData) => ({
-        ...prevInvoiceData,
-        [e.target.name]: e.target.value,
-      }));
-      // console.log(invoiceData);
-    }
-  
+    setInvoiceData((prevInvoiceData) => ({
+      ...prevInvoiceData,
+      [name]: value,
+    }));
 
-  const ownerData ={
-    ownerEmail: Cookies.get("email"),
-    
+    // Clear the error for the corresponding input
+    setFormErrors((prevErrors) => ({
+      ...prevErrors,
+      [name]: false,
+    }));
   }
 
-  async function createInvoice() {
-    try{
-      const { data, error } = await supabase
-      .from('invoices')
-      .insert([
-        { userId: Cookies.get('userID'), invoiceData: invoiceData, invoice_id: invoiceId },
-      ])
-      .select()
-      Swal.fire({
-        position: 'center',
-        icon: 'success',
-        title: 'Your Invoice has been created.',
-        showConfirmButton: false,
-        timer: 1500
-      })
-    }
-    catch(error){
-      Swal.fire({
-        position: 'center',
-        icon: 'error',
-        title: 'Oops...Something went wrong!',
-        showConfirmButton: false,
-        timer: 1500
-      })
-    }
+  const ownerData = {
+    ownerEmail: Cookies.get("email"),
+  };
 
+  async function createInvoice() {
+    try {
+      const { data, error } = await supabase
+        .from("invoices")
+        .insert([
+          {
+            userId: Cookies.get("userID"),
+            invoiceData: invoiceData,
+            invoice_id: invoiceId,
+          },
+        ])
+        .select();
+      Swal.fire({
+        position: "center",
+        icon: "success",
+        title: "Your Invoice has been created.",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    } catch (error) {
+      Swal.fire({
+        position: "center",
+        icon: "error",
+        title: "Oops...Something went wrong!",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    }
   }
 
   return (
@@ -94,31 +111,60 @@ const InvoiceAddPage = () => {
               />
             </div>
 
-            <Textinput
-              label="Recipient's Name"
-              type="text"
-              placeholder="Add Recipient's Name"
-              onChange={(e) => {
-                handleChange(e);
-                e.target.name = "recipientName";
-              }}
+            <div>
+              <Textinput
+                label="Recipient's Name"
+                type="text"
+                placeholder="Add Recipient's Name"
+                onChange={(e) => {
+                  handleChange(e);
+                  e.target.name = "recipientName";
+                }}
+                error={formErrors.recipientName}
+              />
+              {formErrors.recipientName && (
+                <p className="text-red-500 mt-2">
+                  Recipient's Name is required.
+                </p>
+              )}
+            </div>
 
-            />
-            <Textinput label="Recipient's Phone" type="text" placeholder="Add Recipient's phone" 
-              onChange={(e) => {
-                handleChange(e);
-                e.target.name = "recipientPhone";
-              }}
-            />
-            <Textinput
-              label="Recipient's Email"
-              type="email"
-              placeholder="Add recipient's email"
-              onChange={(e) => {
-                handleChange(e);
-                e.target.name = "recipientEmail";
-              }}
-            />
+            <div>
+              <Textinput
+                label="Recipient's Phone"
+                type="text"
+                placeholder="Add Recipient's phone"
+                onChange={(e) => {
+                  handleChange(e);
+                  e.target.name = "recipientPhone";
+                }}
+                error={formErrors.recipientPhone}
+              />
+              {formErrors.recipientPhone && (
+                <p className="text-red-500 mt-2">
+                  Recipient's Phone is required.
+                </p>
+              )}
+            </div>
+
+            <div>
+              <Textinput
+                label="Recipient's Email"
+                type="email"
+                placeholder="Add recipient's email"
+                onChange={(e) => {
+                  handleChange(e);
+                  e.target.name = "recipientEmail";
+                }}
+                error={formErrors.recipientEmail}
+              />
+              {formErrors.recipientEmail && (
+                <p className="text-red-500 mt-2">
+                  Recipient's Email is required.
+                </p>
+              )}
+            </div>
+
             <div className="lg:col-span-2 col-span-1">
               <Textarea
                 label="Recipient's Address"
@@ -126,38 +172,70 @@ const InvoiceAddPage = () => {
                 placeholder="Add Recipient's Address"
                 rows="2"
                 onChange={(e) => {
-                handleChange(e);
-                e.target.name = "recipientAddress";
-              }}
+                  handleChange(e);
+                  e.target.name = "recipientAddress";
+                }}
+                error={formErrors.recipientAddress}
               />
+              {formErrors.recipientAddress && (
+                <p className="text-red-500 mt-2">
+                  Recipient's Address is required.
+                </p>
+              )}
             </div>
           </div>
+
           <div className="grid lg:grid-cols-2 grid-cols-1 gap-5">
             <div className="lg:col-span-2 col-span-1 text-slate-900 text-base dark:text-slate-300 font-medium">
               Owner info
             </div>
 
-            <Textinput label="Name" type="text" placeholder="Add your name"
-             onChange={(e) => {
-                handleChange(e);
-                e.target.name = "ownerName";
-              }}
-             />
-            <Textinput label="Phone" type="text" placeholder="Add your phone
-            " onChange={(e) => {
-                handleChange(e);
-                e.target.name = "ownerPhone";
-              }} />
+            <div>
+              <Textinput
+                label="Name"
+                type="text"
+                placeholder="Add your name"
+                onChange={(e) => {
+                  handleChange(e);
+                  e.target.name = "ownerName";
+                }}
+                error={formErrors.ownerName}
+              />
+              {formErrors.ownerName && (
+                <p className="text-red-500 mt-2">Owner's Name is required.</p>
+              )}
+            </div>
+
+            <div>
+              <Textinput
+                label="Phone"
+                type="text"
+                placeholder="Add your phone"
+                onChange={(e) => {
+                  handleChange(e);
+                  e.target.name = "ownerPhone";
+                }}
+                error={formErrors.ownerPhone}
+              />
+              {formErrors.ownerPhone && (
+                <p className="text-red-500 mt-2">Owner's Phone is required.</p>
+              )}
+            </div>
+
             <div className="lg:col-span-2 col-span-1">
               <Textinput
                 label="Ower's Email"
                 type="email"
                 placeholder="Add your email"
                 onChange={(e) => {
-                handleChange(e);
-                e.target.name = "ownerEmail";
-              }}
+                  handleChange(e);
+                  e.target.name = "ownerEmail";
+                }}
+                error={formErrors.ownerEmail}
               />
+              {formErrors.ownerEmail && (
+                <p className="text-red-500 mt-2">Owner's Email is required.</p>
+              )}
             </div>
 
             <div className="lg:col-span-2 col-span-1">
@@ -166,34 +244,86 @@ const InvoiceAddPage = () => {
                 type="text"
                 placeholder="Add your address"
                 rows="2"
+                error={formErrors.ownerAddress}
+                onChange={(e) => {
+                  handleChange(e);
+                  e.target.name = "ownerAddress";
+                }}
               />
+              {formErrors.ownerAddress && (
+                <p className="text-red-500 mt-2">
+                  Owner's Address is required.
+                </p>
+              )}
             </div>
           </div>
         </div>
         <div className="my-6">
           <Repeater sendData={sendData} />
         </div>
-        <Textarea
-          label="Additional note"
-          type="text"
-          rows="2"
-          placeholder="Note"
-          className="lg:w-1/2"
-          onChange={(e) => {
-                handleChange(e);
-                e.target.name = "additionalNote";
-              }}
-        />
+        <div>
+          <Textarea
+            label="Additional note"
+            type="text"
+            rows="2"
+            placeholder="Note"
+            className="lg:w-1/2"
+            onChange={(e) => {
+              handleChange(e);
+              e.target.name = "additionalNote";
+            }}
+            error={formErrors.additionalNote}
+          />
+          {formErrors.additionalNote && (
+            <p className="text-red-500 mt-2">Additional Note is required.</p>
+          )}
+        </div>
+
         <div className="ltr:text-right rtl:text-left space-x-3 rtl:space-x-reverse mt-4">
-        <Button text="Back" className="btn-dark" onClick={()=>{
-            window.history.back();
-            // console.log(invoiceData);
-          }} />
-          <Button text="Save" className="btn-dark" onClick={()=>{
-            invoiceData.itemData = itemData;
-            createInvoice();
-            // console.log(invoiceData);
-          }} />
+          <Button
+            text="Back"
+            className="btn-dark"
+            onClick={() => {
+              window.history.back();
+            }}
+          />
+          <Button
+            text="Save"
+            className="btn-dark"
+            onClick={() => {
+              // Check for form completeness
+              const requiredFields = [
+                "recipientName",
+                "recipientPhone",
+                "recipientEmail",
+                "recipientAddress",
+                "ownerName",
+                "ownerPhone",
+                "ownerEmail",
+                "ownerAddress",
+                "additionalNote",
+              ];
+              let hasErrors = false;
+
+              const updatedErrors = { ...formErrors };
+
+              requiredFields.forEach((field) => {
+                if (!invoiceData[field]) {
+                  updatedErrors[field] = true;
+                  hasErrors = true;
+                }
+              });
+
+              if (hasErrors) {
+                setFormErrors(updatedErrors);
+                return;
+              }
+
+              // Form is valid, proceed with creating the invoice
+              invoiceData.itemData = itemData;
+              createInvoice();
+            }}
+          />
         </div>
       </Card>
     </div>
